e4fa1ba7cdd9011ad9ffecab4a9dde70777674ef
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-05 23:35:59 +0545

    Config:Auth updated with admins

--- database/migrations/2020_09_05_174442_create_admins_table.php
+++ database/migrations/2020_09_05_174442_create_admins_table.php
@@ -17,6 +17,7 @@ class CreateAdminsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_super')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
