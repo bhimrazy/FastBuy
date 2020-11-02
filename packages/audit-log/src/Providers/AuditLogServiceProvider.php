696178e781fd4c66a47010acb5a4cd048818d3e8
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-02 23:20:34 +0545

    AuditLogServiceProvider added

---
+++ packages/audit-log/src/Providers/AuditLogServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace FastBuy\AuditLog\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuditLogServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+    }
+}
