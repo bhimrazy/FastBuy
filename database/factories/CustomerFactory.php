00fd2f6b275ad6dafff46aa971165eea6b38a71f
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-03 19:11:20 +0545

    customer factory updated

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,19 +1,35 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\User;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'first_name' => $faker->firstName,
-        'last_name'=>$faker->lastName,
-        'email' => $faker->email,
-        'mobile'=>$faker->phoneNumber,
-        'email_verified_at' => now(),
-        'password'=>Hash::make('password'),
-        'type'=>'customer',
-        'remember_token' => Str::random(10),
-    ];
-});
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name'=>$this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'mobile'=>$this->faker->phoneNumber,
+            'email_verified_at' => now(),
+            'password'=>Hash::make('password'),
+            'type'=>'customer',
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
