9acd1f7beef32399cc2805829c7647e6511b408d
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-03 19:08:35 +0545

    category factory updated

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,14 +1,30 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Category;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Category::class, function (Faker $faker) {
-    $title = $faker->unique()->randomElement(['Home Appliances','Kitchen Appliances','Smart Home Appliances','Large Appliances','Small Appliances','Cookware','Drinkware']);
-    return [
-        'title'=>ucwords(strtolower($title)),
-        'slug'=>slugify($title),
-    ];
-});
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->unique()->randomElement(['Home Appliances','Kitchen Appliances','Smart Home Appliances','Large Appliances','Small Appliances','Cookware','Drinkware']);
+        return [
+            'title'=>ucwords(strtolower($title)),
+            'slug'=>slugify($title),
+        ];
+    }
+}
