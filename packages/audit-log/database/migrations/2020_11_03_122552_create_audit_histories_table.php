ee7c761ec7a2ca307df85065cbfd21cca5e2e251
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-03 18:13:37 +0545

    fields added under auithistory table

--- packages/audit-log/database/migrations/2020_11_03_122552_create_audit_histories_table.php
+++ packages/audit-log/database/migrations/2020_11_03_122552_create_audit_histories_table.php
@@ -15,6 +15,17 @@ class CreateAuditHistoriesTable extends Migration
     {
         Schema::create('audit_histories', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned()->references('id')->on('users')->index();
+            $table->integer('admin_id')->unsigned()->references('id')->on('admins')->index();
+            $table->string('module', 60)->index();
+            $table->text('request')->nullable();
+            $table->string('action', 120);
+            $table->text('user_agent')->nullable();
+            $table->string('ip_address', 25)->nullable();
+            $table->integer('reference_user')->unsigned();
+            $table->integer('reference_id')->unsigned();
+            $table->string('reference_name', 255);
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
