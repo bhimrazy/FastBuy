f91b842378db2003928c46bcca4c3956fc2542f7
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-04 19:54:11 +0545

    LogHistory model,migration and controller created

---
+++ database/migrations/2020_11_04_140517_create_log_histories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_histories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->references('id')->on('users')->index();
+            $table->bigInteger('admin_id')->unsigned()->references('id')->on('admins')->index();
+            $table->string('module', 60)->index();
+            $table->text('request')->nullable();
+            $table->string('action', 120);
+            $table->text('user_agent')->nullable();
+            $table->string('ip_address', 25)->nullable();
+            $table->integer('reference_user')->unsigned();
+            $table->integer('reference_id')->unsigned();
+            $table->string('reference_name', 255);
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_histories');
+    }
+}
