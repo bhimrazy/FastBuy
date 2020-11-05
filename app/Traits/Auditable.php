9774c8575e71a1e392cc7469bd9e25a560de1248
bhimrazy <bhimrajyadav977@gmail.com>
2020-11-05 21:44:38 +0545

    auditable trait updated

--- app/Traits/Auditable.php
+++ app/Traits/Auditable.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Traits;
-
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+
 
 trait Auditable
-{
+{   protected static $request;
     public static function bootAuditable()
     {
+        static::$request = App::make(Request::class);
         static::created(function (Model $model) {
             self::audit('created', $model);
         });
@@ -21,8 +24,25 @@ trait Auditable
         });
     }
 
-    protected static function audit($description, $model)
+    protected static function audit($action, $model)
     {
+        $data = [
+            'user_agent'     => Auditable::$request->userAgent(),
+            'ip_address'     => Auditable::$request->ip,
+            'module'         => get_class($model) ?? null,
+            'action'         => $action,
+            'activity'         =>$action.$model,
+            'user_id'        => auth()->guard('admin')->user()==null?auth()->id() : null,
+            'admin_id'        =>auth()->guard('admin')->user()!=null?auth()->guard('admin')->id():null,
+            'reference_user' => auth()->guard('admin')->user()!=null?auth()->guard('admin')->id():auth()->id(),
+            'reference_id'   => $model->id ?? null,
+            'reference_name' => auth()->guard('admin')->user()!=null?auth()->guard('admin')->user()->getFullName():auth()->user()->getFullName(),
+            'type'           => 'info',
+            'url'           => Auditable::$request->fullUrl(),
+        ];
+        if (!in_array($action, ['loggedIn', 'password'])) {
+            $data['request'] = json_encode(Auditable::$request->input());
+        }
         \App\LogHistory::create([
             'description'  => $description,
             'subject_id'   => $model->id ?? null,
